80b3aa4803c359359c4a6bb82d3f8a197347ba03
Abdullah Albagshi <abdullah.albagshi@tuwaiq.edu.sa>
2021-07-28 00:11:23 +0300

    feat: about us page

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,13 @@
+<x-news>
+    <h1>
+        Welcome to <span class="text-blue-900"> News App</span>, your number one source for all The <span
+            class="text-blue-900">News</span>.
+    </h1>
+    <h1>We're dedicated to providing you the very best of <span class="text-blue-900">News</span> ,with an emphasis on
+        all type of
+        <span class="text-blue-900">News</span>.
+    </h1>
+    <h1>Founded in <span class="text-blue-900">{{ now()->year }}</span> by <span class="text-blue-900">Abdullah
+            Albagshi</span>,
+    </h1>
+</x-news>
