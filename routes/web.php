e603a974cc4af6804692074bb0724c3755fe620c
Abdullah Albagshi <abdullah.albagshi@tuwaiq.edu.sa>
2021-07-28 00:20:03 +0300

    feat: add contact us , about us routes , add news count to dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ContactController;
 use App\Models\Category;
 use App\Models\Comment;
 use App\Models\News;
@@ -28,17 +29,20 @@ Route::get('/dashboard', function () {
     return view('dashboard', [
         'category' => Category::newsCount(),
         'comments' => Comment::countComments(),
+        'totalNews' =>  News::count()
     ]);
 })->middleware(['auth'])->name('dashboard');
 
 
 Route::prefix('dashboard')->middleware(['auth'])->group(function () {
-    Route::resource('news', NewsController::class, ['except' => ['index', 'show']]);
+    Route::resource('news', NewsController::class, ['except' => ['index', 'show','allnews']]);
     Route::get('/news/{news:slug}/edit', [NewsController::class, 'edit'])->name('dashboard.news.edit');
     Route::resource('comments', CommentController::class, ['except' => ['show', 'create']]);
     Route::get('/news', function () {
         return view('dashboard-news', ['news' => News::with('category', 'author', 'comments')->latest()->paginate(10)]);
     })->name('dashboard.news');
+
+    Route::resource('contact', ContactController::class, ['except' => ['create','store']]);
 });
 
 Route::get('/', [NewsController::class, 'index'])->name('news');
@@ -46,6 +50,10 @@ Route::get('news/{news:slug}', [NewsController::class, 'show'])->name('news.show
 
 Route::post('news/{news:slug}/comments', [CommentController::class, 'store'])->name('news.comments.store');
 
+Route::get('contact-us', [ContactController::class, 'create'])->name('contact');
+Route::post('contact-us', [ContactController::class, 'store'])->name('contact.store');
+
+Route::get('/about-us', fn()=> view('about-us'))->name('about');
 
 Route::get('allnews', [NewsController::class, 'allNews'])->name('allnews');
 
