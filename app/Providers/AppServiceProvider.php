b3567d3b687998d29bf4d4c5b9a78f9be336a01f
Abdullah Albagshi <abdullah.albagshi@tuwaiq.edu.sa>
2021-07-28 10:22:02 +0300

    setup for digital ocean deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if(env('APP_ENV')==='local'){
+            URL::forceScheme('https');
+        }
     }
 }
